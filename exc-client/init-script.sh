c1ba4a4a12b3c73a975f18f295cc91f5d807ac5e
Marc Ahlgrim <marc@onemarcfifty.com>
2023-03-19 17:51:39 +0100

    added apt update
    Signed-off-by: Marc Ahlgrim <marc@onemarcfifty.com>

--- exc-client/init-script.sh
+++ exc-client/init-script.sh
@@ -27,6 +27,7 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
+apt update
 apt -y install code 
 
 USERHOME=$(grep -i ^$NONROOTUSER: /etc/passwd | cut -d ":" -f 6)
